e4e528e62b3f627b68c715ddd8f8a22dd813afdc
djwa <hello@djwa.pl>
2015-08-26 15:39:51 +0100

    bootstrap integration

--- README.md
+++ README.md
@@ -8,16 +8,18 @@ CONTAINS
 #What's in it?
 - HTML5 basic skeleton,
 - CSS3 basic styles,
+- jQuery,
 - Twitter Bootstrap,
 - Modernizr,
 - HTML5 manifest and localstorage entry,
-- Grunt.js includes following task:
--- copy clean, minified files to the build version,
--- HTML and CSS minification,
--- images minification,
--- images sprite tool,
--- LESS compiler,
--- live server to run project,
--- watch task to automaticly update build
 
-TODO: implement modernizr, HTML5 offile manifest, HTML5 localstorage entry
+#Grunt js
+- copy clean, minified files to the build version,
+- HTML and CSS minification,
+- images minification,
+- images sprite tool,
+- LESS compiler,
+- live server to run project,
+- watch task to automatically update build
+
+TODO:  HTML5 offile manifest, HTML5 localstorage entry
--- config/copy.js
+++ config/copy.js
@@ -10,6 +10,7 @@ module.exports = {
                     //'images/*', //commented as other tasks are resposible for coping images
                     'views/**/*.html',
                     'js/scripts.js',
+                    'js/lib/**/*.js',
                     'fonts/**/.*'
                 ]
             }, {
@@ -18,12 +19,17 @@ module.exports = {
                 dest: '<%= srcConfig.dist %>/images',
                 src: ['sprite/*', 'logos/*']
             },
-            //{
-            //    expand: true,
-            //    cwd: 'bower_components'
-            //    //todo: where JS goes
-            //},
             {
+                expand: true,
+                cwd: 'bower_components/bootstrap/dist',
+                src: 'js/bootstrap.min.js',
+                dest: '<%= srcConfig.dist %>'
+            }, {
+                expand: true,
+                cwd: 'bower_components/jquery/dist',
+                src: 'jquery.min.js',
+                dest: '<%= srcConfig.dist %>/js'
+            }, {
                 expand: true,
                 cwd: 'bower_components/bootstrap/dist',
                 src: 'fonts/*',
