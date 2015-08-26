1938fbf60adc271f999adb67439ff32c158f2422
djwa <hello@djwa.pl>
2015-08-26 16:48:23 +0100

    manifest update

--- config/manifest.js
+++ config/manifest.js
@@ -1,30 +1,28 @@
 module.exports = function (grunt, config) {
     return {
-        //pkg: grunt.file.readJSON('package.json'),
-        manifest: {
-            generate: {
-                options: {
-                    basePath: '<%= srcConfig.dist %>',
-                    cache: ['<%= srcConfig.dist %>/js/scripts.js', '<%= srcConfig.dist %>/styles/style.css'],
-                    network: ['http://*', 'https://*'],
-                    fallback: ['<%= srcConfig.dist %>/offline.html'],
-                    preferOnline: true,
-                    headcomment: "Awesome manifest",
-                    verbose: true,
-                    timestamp: true,
-                    hash: true,
-                    master: ['<%= srcConfig.src %>/index.html'],
-                    process: function (path) {
-                        return path.substring('<%= srcConfig.dist %>/'.length);
-                    }
-                },
-                src: [
-                    '<%= srcConfig.dist %>/*.html',
-                    '<%= srcConfig.dist %>/js/*.js',
-                    '<%= srcConfig.dist %>/styles/*.css'
-                ],
-                dest: 'manifest.appcache'
-            }
+        generate: {
+            options: {
+            //    basePath: '<%= srcConfig.dist %>'
+            //    cache: ['<%= srcConfig.dist %>/js/scripts.js', '<%= srcConfig.dist %>/styles/style.min.css'],
+            //    network: ['http://*', 'https://*'],
+            //    fallback: ['<%= srcConfig.dist %>/offline.html'],
+            //    preferOnline: true,
+                headcomment: "Awesome manifest"
+            //    verbose: true,
+            //    timestamp: true,
+            //    hash: true,
+            //    master: ['<%= srcConfig.src %>/index.html'],
+            //    process: function (path) {
+            //        return path.substring('<%= srcConfig.dist %>/'.length);
+            //    }
+            },
+            src: [
+                '<%= srcConfig.dist %>/*.html',
+                '<%= srcConfig.dist %>/js/*.js',
+                '<%= srcConfig.dist %>/images/**/*',
+                '<%= srcConfig.dist %>/styles/*.css'
+            ],
+            dest: '<%= srcConfig.dist %>/manifest.appcache'
         }
     }
 };
